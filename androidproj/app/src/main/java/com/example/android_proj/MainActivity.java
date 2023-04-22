d090b215e6211d539c38cfa4002d865e4a9d6c62
emilyvo <evo3@vols.utk.edu>
2023-04-22 18:06:22 -0400

    update

--- androidproj/app/src/main/java/com/example/android_proj/MainActivity.java
+++ androidproj/app/src/main/java/com/example/android_proj/MainActivity.java
@@ -10,14 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button myButton = findViewById(R.id.my_button);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MyNewActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
