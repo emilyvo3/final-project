bf16cc1b44fafd5145434c08abc921d0a5daa6fa
Dhwani Patel <dpate116@vols.utk.edu>
2023-04-28 10:44:38 -0400

    update

--- androidproj/app/src/main/java/com/example/android_proj/book_appt.java
+++ androidproj/app/src/main/java/com/example/android_proj/book_appt.java
@@ -3,6 +3,8 @@ package com.example.android_proj;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class book_appt extends AppCompatActivity {
 
--- androidproj/app/src/main/java/com/example/android_proj/confirmation.java
+++ androidproj/app/src/main/java/com/example/android_proj/confirmation.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class confirmation extends AppCompatActivity {
@@ -17,8 +18,8 @@ public class confirmation extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(confirmation.this, NextActivity.class);
-                startActivity(intent);
+              //  Intent intent = new Intent(confirmation.this, NextActivity.class);
+              //  startActivity(intent);
             }
         });
 
