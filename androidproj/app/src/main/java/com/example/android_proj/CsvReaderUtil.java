0c1d8fab322db3f3ce94ffa345678b73b2565b87
Dhwani Patel <dpate116@vols.utk.edu>
2023-05-08 10:36:54 -0400

    update

--- androidproj/app/src/main/java/com/example/android_proj/CsvReaderUtil.java
+++ androidproj/app/src/main/java/com/example/android_proj/CsvReaderUtil.java
@@ -5,32 +5,47 @@ import com.opencsv.exceptions.CsvException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CsvReaderUtil {
     public static void readCsv(String filePath, String coursesList) throws IOException {
         try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            Map<String, List<Map<String, String>>> multimap = new HashMap<>();
+
+// Read in data from file or other source, and split into rows
             List<String[]> rows = reader.readAll();
 
-            // Iterate over each row
+// Loop through rows and split into columns
             for (String[] row : rows) {
-                // Access individual columns using array indices
-                String course = row[0];
-                String student = row[2];
-                String location = row[3];
-                String time = row[4];
+                String[] columns = row.toString().split(",");
 
-                // Check if course matches coursesList
-                if (course.equals(coursesList)) {
-                    // Process the data as needed; display column info
-                    System.out.println("Course: " + course);
-                    System.out.println("Student: " + student);
-                    System.out.println("Location: " + location);
-                    System.out.println("Time: " + time);
+                // Create a new Map to hold the data for this row
+                Map<String, String> rowMap = new HashMap<>();
+                rowMap.put("instructorLast", columns[1]);
+                rowMap.put("instructorFirst", columns[2]);
+                rowMap.put("location1", columns[4]);
+                // ... and so on for all columns
+
+                // Get the course code for this row
+                String courseCode = columns[0];
+
+                // If this course code isn't already in the multimap, add it with an empty List
+                if (!multimap.containsKey(courseCode)) {
+                    multimap.put(courseCode, new ArrayList<>());
                 }
+
+                // Add the row data to the List for this course code
+                multimap.get(courseCode).add(rowMap);
             }
+
         } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
+
     }
+
+
 }
